22b54da4059a0a23a51ffd43f6fab88e970c7b8c
George Flores <georgeflores886@gmail.com>
2020-09-11 12:13:14 -0400

    updated vowels.js

--- vowels/vowels.js
+++ vowels/vowels.js
@@ -4,9 +4,13 @@ the number of vowels in the string
 */
 
 const findVowels = str => {
+    //created variable for count vowels.
     let count = 0;
-    const vowels= ['a', 'e', 'i', 'o', 'u']
+    //created an array for vowels both lowercase and uppercase
+    const vowels= ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    //iterate through the string to find the vowels
     for(let i of str.toLowerCase()) {
+        //if one of the vowels is in the string, we add 1 to the counter
         if(vowels.includes(i)){
             count++
         }
@@ -15,3 +19,4 @@ const findVowels = str => {
 }
 
 console.log(findVowels('Hey there friend!'))
+console.log(findVowels('hEllO worLD'))
